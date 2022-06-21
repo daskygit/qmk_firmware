37eb7b9785ae14d8a3005281de9f24f9c5d00fd8
Dasky <32983009+daskygit@users.noreply.github.com>
2022-06-21 17:30:35 +0100

    userspace

---
+++ keyboards/crkbd/keymaps/dasky_blip/rules.mk
@@ -0,0 +1,19 @@
+# MCU name
+MCU = STM32G474
+
+# Bootloader selection
+BOOTLOADER = stm32-dfu
+
+RGB_MATRIX_ENABLE = yes
+USBPD_ENABLE = yes
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+NKRO_ENABLE = yes
+
+SERIAL_DRIVER = usart
+WS2812_DRIVER = pwm
+EEPROM_DRIVER = i2c
+
+EXTRAFLAGS += -O2
+
+USER_NAME := dasky
---
+++ keyboards/reverb/matrix.c
@@ -0,0 +1,82 @@
+// Copyright 2022 Dasky (@daskygit)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "matrix.h"
+#include "gpio.h"
+#include "string.h"
+
+const pin_t matrix_read_pins[] = {B3, B4, B5, B6, B7, B8};
+const pin_t matrix_out_pins[]  = {B12, B13, B14, B15, A8};
+
+#define PORTB_READ_BITMASK (1 << 3 | 1 << 4 | 1 << 5 | 1 << 6 | 1 << 7 | 1 << 8)
+#define PORTB_OUT_BITMASK (1 << 12 | 1 << 13 | 1 << 14 | 1 << 15)
+
+void matrix_all_pins_high(void) {
+    setPinInputHigh(A8);
+    palSetGroupMode(GPIOB, (PORTB_OUT_BITMASK | PORTB_READ_BITMASK), 0, PAL_MODE_INPUT_PULLUP);
+}
+
+void matrix_all_pins_low(void) {
+    setPinInputLow(A8);
+    palSetGroupMode(GPIOB, (PORTB_OUT_BITMASK | PORTB_READ_BITMASK), 0, PAL_MODE_INPUT_PULLDOWN);
+}
+
+void matrix_init_custom(void) {
+    matrix_all_pins_high();
+}
+
+void matrix_scan_high(matrix_row_t current_matrix[], matrix_row_t temp_matrix[]) {
+    for (uint8_t current_row = 0; current_row < 5; current_row++) {
+        pin_t output_pin = matrix_out_pins[current_row];
+        setPinOutput(output_pin);
+        writePinLow(output_pin);
+        while (readPin(matrix_out_pins[current_row]) != 0)
+            ;
+        uint16_t port_b = palReadPort(GPIOB);
+        setPinInputHigh(output_pin);
+        temp_matrix[current_row] = ~((port_b & PORTB_READ_BITMASK) >> 3);
+    }
+}
+
+void matrix_scan_low(matrix_row_t current_matrix[], matrix_row_t temp_matrix[]) {
+    for (uint8_t current_row = 0; current_row < 5; current_row++) {
+        pin_t output_pin = matrix_out_pins[current_row];
+        setPinOutput(output_pin);
+        writePinHigh(output_pin);
+        while (readPin(matrix_out_pins[current_row]) != 1)
+            ;
+        uint16_t port_b = palReadPort(GPIOB);
+        setPinInputLow(output_pin);
+        temp_matrix[current_row + 5] = ((port_b & PORTB_READ_BITMASK) >> 3);
+    }
+}
+
+bool matrix_scan_custom(matrix_row_t current_matrix[]) {
+    bool                matrix_has_changed           = false;
+    static matrix_row_t temp_matrix[MATRIX_ROWS]     = {0};
+    static enum { UNKNOWN, LOW, HIGH } next_scan_dir = UNKNOWN;
+
+    switch (next_scan_dir) {
+        case UNKNOWN:
+            matrix_all_pins_high();
+        case HIGH:
+            matrix_scan_high(current_matrix, temp_matrix);
+            matrix_all_pins_low();
+            matrix_scan_low(current_matrix, temp_matrix);
+            next_scan_dir = LOW;
+            break;
+        case LOW:
+            matrix_scan_low(current_matrix, temp_matrix);
+            matrix_all_pins_high();
+            matrix_scan_high(current_matrix, temp_matrix);
+            next_scan_dir = HIGH;
+            break;
+    }
+
+    if (memcmp(temp_matrix, current_matrix, sizeof(temp_matrix))) {
+        memcpy(current_matrix, temp_matrix, sizeof(temp_matrix));
+        matrix_has_changed = true;
+    }
+
+    return matrix_has_changed;
+}
