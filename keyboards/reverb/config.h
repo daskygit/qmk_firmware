eb9fcef7117e51253ba87a6c57e9f0c2357c7f3d
Dasky <32983009+daskygit@users.noreply.github.com>
2022-09-17 00:18:09 +0100

    Reverb init

---
+++ keyboards/reverb/config.h
@@ -0,0 +1,41 @@
+// Copyright 2022 Dasky (@daskygit)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define SPI_DRIVER SPID1
+#define SPI_SCK_PIN A5
+#define SPI_SCK_PAL_MODE 5
+#define SPI_MOSI_PIN A7
+#define SPI_MOSI_PAL_MODE 5
+#define SPI_MISO_PIN A6
+#define SPI_MISO_PAL_MODE 5
+
+#define DISPLAY_RST A1
+#define DISPLAY_CS A2
+#define DISPLAY_DC A3
+
+#define QUANTUM_PAINTER_SUPPORTS_256_PALETTE true
+
+#define WS2812_PWM_DRIVER PWMD2
+#define WS2812_PWM_CHANNEL 3
+#define WS2812_PWM_PAL_MODE 1
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM1
+#define WS2812_DMA_CHANNEL 3
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define DEBUG_MATRIX_SCAN_RATE
